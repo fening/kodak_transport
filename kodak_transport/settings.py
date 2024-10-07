b61d75445d8cceee47e5edb0151b475f50497d8d
Fening Okai <feningokai@outlook.com>
2024-10-07 15:51:01 -0400

    v1

--- kodak_transport/settings.py
+++ kodak_transport/settings.py
@@ -28,12 +28,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qm1jgrdbbbps30_$f(v1+uz-r@rkdg9!n(=61sz)%#mdizi#mg'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if ENVIRONMENT == 'development':
+    DEBUG = True
+else:
+    DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost', 'https://kodaklogistics.up.railway.app/']
+
+CSRF_TRUSTED_ORIGINS = ['https://https://kodaklogistics.up.railway.app/']
+
+INTERNAL_IPS = ('127.0.0.1',
+                'localhost:8080')
 
 
 # Application definition
