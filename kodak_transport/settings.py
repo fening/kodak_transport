4d9c3772131b074db6bd0728be7f2f2b969d8eab
Fening Okai <feningokai@outlook.com>
2024-11-09 11:15:44 -0500

    v1

--- kodak_transport/settings.py
+++ kodak_transport/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qm1jgrdbbbps30_$f(v1+uz-r@rkdg9!n(=61sz)%#mdizi#mg"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if ENVIRONMENT == 'development':
@@ -120,7 +120,7 @@ POSTGRES_LOCALLY = env.bool('POSTGRES_LOCALLY', default=False)
 # Database settings
 if ENVIRONMENT == 'production' or POSTGRES_LOCALLY:
     DATABASES = {
-        'default': dj_database_url.parse('postgresql://postgres:JmfgPmeYxZrCSGRCvzxCOBGFmbWVzSmG@autorack.proxy.rlwy.net:57058/railway')
+        'default': dj_database_url.parse(env('DATABASE_URL'))
     }
     print("Using production/POSTGRES_LOCALLY database:", DATABASES['default'])
 else:
